bb705e4837b1d157e59022e26ffe6001b7dc7c02
Alok Naushad <alokme123@gmail.com>
2022-09-25 02:35:45 +0530

    fix:staticfiles

--- thanimaBackend/settings.py
+++ thanimaBackend/settings.py
@@ -59,8 +59,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -180,9 +180,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles/"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "staticfiles/"
+# ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
